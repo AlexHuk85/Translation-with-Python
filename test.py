1130af75a49a582569e48a79c6f1061ad4619d1e
AlexHuk85 <alex.huk85@gmail.com>
2020-01-20 18:48:16 +1100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-from translate import Translator
+from translate import Translator #<-- translate 3.5.0 from pypi
 
 with open('new.txt', 'r') as my_file:
 
     text = my_file.read()
-
-    translator = Translator(to_lang="vi")
+    
+    #---------------Translate part-----------------------#
+    translator = Translator(to_lang="vi") #<--Can change to other laguages
     translation = translator.translate(text)
 
     print(translation)
